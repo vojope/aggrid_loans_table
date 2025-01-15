ae4c9ad03dfde7c110a4801b2b5083c60c880cf9
vojope <194944115+vojope@users.noreply.github.com>
2025-01-15 13:39:28 +0100

    column menu configurations

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -4,7 +4,7 @@ import { AgGridReact } from "ag-grid-react";
 // import "ag-grid-community/dist/styles/ag-theme-balham.css";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-balham.css";
-import { GridReadyEvent, GridApi, ColumnApi, ColDef } from "ag-grid-community";
+import { GridReadyEvent, GridApi, ColumnApi, ColDef, ColumnMenuTab } from "ag-grid-community";
 import { fetchData, fetchLargeData, Athlete } from "./api";
 
 const columnDefs: ColDef[] = [
@@ -13,6 +13,8 @@ const columnDefs: ColDef[] = [
     field: "id",
     // width: 70,
     pinned: "left",
+    suppressMenu: true,
+    suppressColumnsToolPanel: true,
   },
   {
     headerName: "Athlete",
@@ -20,6 +22,8 @@ const columnDefs: ColDef[] = [
     // width: 150,
     editable: true,
     pinned: "left",
+    suppressMenu: true,
+    suppressColumnsToolPanel: true,
   },
   {
     headerName: "Age",
@@ -71,6 +75,8 @@ const columnDefs: ColDef[] = [
     // width: 100,
     maxWidth: 70,
     pinned: "right",
+    suppressMenu: true,
+    suppressColumnsToolPanel: true,
   },
 ];
 
@@ -86,15 +92,43 @@ function Grid() {
     // fetchLargeData().then((d) => setRowData(d));
   }, []);
 
+  const postProcessPopup = React.useCallback((params) => {
+    // check callback is for menu
+    if (params.type !== 'columnMenu') {
+      return;
+    }
+    // const columnId = params.column ? params.column.getId() : undefined;
+    // if (columnId === 'gold') {
+      const ePopup = params.ePopup;
+      let oldTopStr = ePopup.style.top;
+      // remove 'px' from the string (AG Grid uses px positioning)
+      oldTopStr = oldTopStr.substring(0, oldTopStr.indexOf('px'));
+      const oldTop = parseInt(oldTopStr);
+      const newTop = oldTop + 25;
+      ePopup.style.top = newTop + 'px';
+    // }
+  }, []);
+
   const gridOptions = {
     defaultColDef: {
-      menuTabs: ["columnsMenuTab"],
+      menuTabs: ["columnsMenuTab"] as ColumnMenuTab[],
       // flex: 1,
-      // wrapHeaderText: true,
-      // autoHeaderHeight: true,
+      wrapHeaderText: true,
+      autoHeaderHeight: true,
+      columnsMenuParams: {
+        suppressColumnFilter: true,
+        suppressColumnSelectAll: true,
+        suppressColumnExpandAll: true,
+        suppressSyncLayoutWithGrid: true
+      },
     },
     suppressMovableColumns: true,
+    suppressContextMenu: true,
+    suppressRowClickSelection: true,
+    suppressDragLeaveHidesColumns: true,
+    postProcessPopup: postProcessPopup,
   };
+  
 
   return (
     <div style={{ height: "80vh" }}>
@@ -103,9 +137,6 @@ function Grid() {
         className="ag-theme-balham"
       >
         <AgGridReact
-          rowSelection="multiple"
-          suppressRowClickSelection
-          suppressDragLeaveHidesColumns
           columnDefs={columnDefs}
           onGridReady={onGridReady}
           rowData={rowData}
